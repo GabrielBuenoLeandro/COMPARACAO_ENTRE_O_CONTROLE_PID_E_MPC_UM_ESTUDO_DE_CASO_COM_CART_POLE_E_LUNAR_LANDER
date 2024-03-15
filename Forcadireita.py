8d9880f5fa762c061a27bb3fb3573ae4397da376
GabrielBuenoLeandro <gabrielbuenoleandro00@gmail.com>
2024-03-15 18:26:06 -0300

    Adicao_de_codigo

--- Forcadireita.py
+++ Forcadireita.py
@@ -1,9 +1,13 @@
 import gym
 from matplotlib import pyplot as plt
+import numpy as np
+from scipy.stats import linregress
+
+
+# Gráfico: "Resultados da aplicação de uma força constante à direita." 
 
 env = gym.make("CartPole-v1", render_mode="human")
 env.action_space.seed(42)
-
 observation, info = env.reset(seed=42)
 t = [] # Lista o episódio
 P = [] # Lista para a Posição
@@ -22,8 +26,6 @@ for _ in range(100):
         t.append(_) # Guarda o término de cada episódio
 
 env.close()
-
-
 # Plote dos resultados
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 ax1.plot(P[:t[0]])
@@ -43,3 +45,50 @@ ax4.set_xlabel('Amostras')
 ax4.set_ylabel('rad/s')
 ax4.set_title("Velocidade angular")
 fig.tight_layout()
+plt.show()
+
+# Acelerações linear e angular
+x = np.arange(0, t[0])
+Vl = np.array(V[:t[0]])
+Vt = np.array(Va[:t[0]])
+
+# Aplicando a regressão linear
+resultl = linregress(x, Vl)
+resultt = linregress(x, Vt)
+
+# Exibindo os resultados
+print("Aceleração Linear")
+print("Inclinação:", resultl.slope)
+print("Interceptação:", resultl.intercept)
+print("Coeficiente de correlação:", resultl.rvalue)
+print("Valor p:", resultl.pvalue)
+print("Erro padrão:", resultl.stderr)
+print()
+print("Aceleração Angular")
+print("Inclinação:", resultt.slope)
+print("Interceptação:", resultt.intercept)
+print("Coeficiente de correlação:", resultt.rvalue)
+print("Valor p:", resultt.pvalue)
+print("Erro padrão:", resultt.stderr)
+
+# Gráfico: "Relação entre torque e comprimento, para $m=0,5$,"
+I = np.zeros(10000)
+m = 0.5
+K = []
+x =  abs(resultl.slope)
+o = abs(resultt.slope)
+for l in range(0, 10000):
+    K.append(l/10000)
+    I[l] = (-m*l/10000*x + o*m*(l/10000)**2)*(-o)**(-1)
+item = 6500
+aux = list(I)
+print(K[item], I[item])
+plt.plot(K, I, 'b')
+plt.plot(K[item], I[item], 'or', label=f'Ponto escolhido, I = {np.round(I[item], 5)} $N\cdot m$ e $\ell$ = {K[item]} $m$')
+plt.grid()
+plt.title('Torque x Comprimento')
+plt.xlabel('$\ell[m]$')
+plt.ylabel('$I [N\cdot m]$')
+plt.grid(False)
+plt.legend()
+plt.show()
