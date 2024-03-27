13c1c8a536800664be84e96e56d8095deae27278
GabrielBuenoLeandro <gabrielbuenoleandro00@gmail.com>
2024-03-26 22:04:25 -0300

    Comentando o código do GPC LunarLander

--- Codigos/GPC_LunarLander.py
+++ Codigos/GPC_LunarLander.py
@@ -91,9 +91,6 @@ for i in b:
 x = np.zeros((len(Possibilidades), hc)) # Matriz para organizar todas as possibilidades
 for i in range (0, len(Possibilidades)):
     x[i, :] = np.array(Possibilidades[i])
-# Adição da entrada 0, como forma de preencher a  diferença entre o horizonte de predição e controle
-H = np.zeros((int(4**hc), int(hp-hc)))
-x = np.append(x, H, axis=1)
 
 def MPC(px, py, k, at, v, params, pe):
     """Aplicação do GPC (Controle Preditivo)
@@ -110,117 +107,120 @@ def MPC(px, py, k, at, v, params, pe):
     Returns:
         action (int): Ação que a sonda deve tomar (saída do controlador)
     """
-    ypx = np.array([px[k], px[k-1]]).reshape(-1,1)
-    ypy = np.array([py[k], py[k-1]]).reshape(-1,1)
-    ypt = np.array([at[k], at[k-1]]).reshape(-1,1)
-    ypv = np.array([v[k], v[k-1]]).reshape(-1,1)
-    aux = np.ones(4)
-    p = [-.725, -.125]
-    p = np.poly1d(p)
-    c_x = aux*cx
-    c_y = aux*cy
-    c_v = aux*cv
-    ref = np.ones(1)*int(4**hc)
-    s = np.ones(int(4**hc))*2000
-    aux_x = -sax.dot(ypx)+c_x.reshape(-1,1)
-    aux_y = -say.dot(ypy)+c_y.reshape(-1,1)
-    aux_t = -sat.dot(ypt)
-    aux_v = -say.dot(ypv)+c_v.reshape(-1,1)
-    fx = taxi.dot(tbx)
-    lx = taxi.dot(aux_x)
-    fy = tayi.dot(tby)
-    ly = tayi.dot(aux_y)
-    ft = tati.dot(tbt)
-    lt = tati.dot(aux_t)
-    fv = tavi.dot(tbv)
-    lv = tavi.dot(aux_v)
-    for i in range(0, np.shape(x)[0]):
-        u1 = np.zeros(4)
-        u2 = np.zeros(4)
-        for j in range(0, np.shape(x)[1]):
-            if x[i, j] ==1:
-                u2[j] = 1
-                t = 1
-            if x[i, j] ==3:
-                u2[j] = -1
-                t = 1
-            if x[i, j] ==2:
-                u1[j] = 1
-                t = 1
-        u1 = u1.reshape(-1, 1)
-        u2 = u2.reshape(-1, 1)
-        y_x = (fx.dot(u2)+lx)
-        y_y = (fy.dot(u1)+ly)
-        y_t = (ft.dot(u2)+lt)
-        y_v = (fv.dot(u1)+lv)
-        m = 0
-        g = 1
-        n = 1
-        c = y_x
-        if y_y[0]>y_x[0]:
+    ypx = np.array([px[k], px[k-1]]).reshape(-1,1) # Coleta de p_x(k) e p_x(k-1)
+    ypy = np.array([py[k], py[k-1]]).reshape(-1,1) # Coleta de p_y(k) e p_y(k-1)
+    ypt = np.array([at[k], at[k-1]]).reshape(-1,1) # Coleta de \theta(k) e \theta(k-1)
+    ypv = np.array([v[k], v[k-1]]).reshape(-1,1) # Coleta de v_y(k) e v_y(k-1)
+    aux = np.ones(hp)
+    p = [-.725, -.125] # Setpint para velocidade linear em y (-0,725*p_y - 0.125)
+    p = np.poly1d(p) # Cria polinômio no objeto p (para calcular o setpoint)
+    c_x = aux*cx # Termo constante p_x com dimenção (H_p x 1)
+    c_y = aux*cy # Termo constante p_y com dimenção (H_p x 1)
+    c_v = aux*cv # Termo constante v_y com dimenção (H_p x 1)
+    s = np.ones(int(4**hc))*2000 # Vetor para salvar os valores da função custo, como o valor almejado é o mínimo, salva um valor grande em todo vetor
+    aux_x = -sax.dot(ypx)+c_x.reshape(-1,1) # Cálculo de s_a * p_x mais o termo constante
+    aux_y = -say.dot(ypy)+c_y.reshape(-1,1) # Cálculo de s_a * p_y mais o termo constante
+    aux_t = -sat.dot(ypt)  # Cálculo de s_a * \theta
+    aux_v = -say.dot(ypv)+c_v.reshape(-1,1)  # Cálculo de s_a * v_y mais o termo constante
+    fx = taxi.dot(tbx)  # Matriz G, para obtenção da resposta forçada de p_x
+    lx = taxi.dot(aux_x) # Resposta livre para p_x
+    fy = tayi.dot(tby) # Matriz G, para obtenção da resposta forçada de p_y
+    ly = tayi.dot(aux_y) # Resposta livre para p_y
+    ft = tati.dot(tbt) # Matriz G, para obtenção da resposta forçada de \theta
+    lt = tati.dot(aux_t) # Resposta livre para \theta
+    fv = tavi.dot(tbv) # Matriz G, para obtenção da resposta forçada de v_y
+    lv = tavi.dot(aux_v) # Resposta livre para v_y
+    for i in range(0, np.shape(x)[0]): # Percorre todas as possibilidades de entrada possíveis (4**hc)
+        u1 = np.zeros(4) # Propulsor principal (tamanho do horizonte de predição)
+        u2 = np.zeros(4) # Propulsor auxiliar (tamanho do horizonte de predição)
+        for j in range(0, hc): # Note, que pela formulação, vamos até hc, pois o restante do vetor será zero, esse já definido
+            if x[i, j]==1: # Motor de orientação para a esquerda
+                u2[j] = 1 # Propulsor auxiliar recebe 1
+            if x[i, j]==3: # Motor de orientação para a direita
+                u2[j] = -1 # Propulsor auxiliar recebe -1
+            if x[i, j]==2: # Acionamento do propulsor principal
+                u1[j] = 1 # Propulsor principal recebe 1
+        u1 = u1.reshape(-1, 1) # Transpõe uma matriz linha, logo u1 passa a ser uma matriz coluna
+        u2 = u2.reshape(-1, 1) # Transpõe uma matriz linha, logo u2 passa a ser uma matriz coluna
+        y_x = (fx.dot(u2)+lx)  # Predição de \hat{p}_x = resposta forçada + resosta livre
+        y_y = (fy.dot(u1)+ly) # Predição de \hat{p}_y = resposta forçada + resosta livre
+        y_t = (ft.dot(u2)+lt) # Predição de \hat{\theta} = resposta forçada + resosta livre
+        y_v = (fv.dot(u1)+lv) # Predição de \hat{v}_y = resposta forçada + resosta livre
+        w = np.array([1, 0, 1]) # Pessos iniciais iguais para o setpoint de angular e posicional (1) e nulo para velocidade - Estágio 1
+        c = y_x 
+        if abs(y_y[0])>abs(y_x[0]): # A ideia é acelerar a sonda, já que p_y \approx p_x, pega-se o que possui o maior termo inicial
             c = y_y
-        ref = -p(np.abs(c))
-        if y_v[0]<-ref[0]*.543:  
-            m = params[0] 
-            g = params[1] 
-            n = params[2]  
-        if np.sum(pe)==2:
-            g=0
-        s[i] = sum(abs(n*(y_x[:]-y_y[:])+m*(y_v[:]+ref[:])-g*(y_t[:])))
-    s = list(s)
-    u = s.index(min(s))
-    action = x[u, 0]
-    if sum(pe)!=0 and px[k]>-.1 and px[k]<.1:
-        action = 0 
-    return int(action)
+        ref = -p(np.abs(c)) # O polinômio referente o setpoint da velocidade recebe valores numéricos
+        if y_v[0]<-ref[0]*.543:  # Esse if permite intercalar os dois estágios, se True, o estágio 1 atua 
+            w = np.array([params[2], params[0], params[1]]) # Pesos calculados pelo algoritmo Subida de Encosta - Estágio 2
+        if np.sum(pe)==2: # Verifica se ambos os pés estão em solo lunar
+            w[2] = 0 # Caso afirmativo, o peso do setpoint angular é zero
+        s[i] = sum(abs(w[0]*(y_x[:]-y_y[:])+w[1]*(y_v[:]+ref[:])-w[2]*(y_t[:]))) # Função Custo a ser minimizada
+    action = x[np.argmin(s), 0] # Retorna com a primeira entrada do vetor de entra da que minimiza a Função Custo
+    if sum(pe)==2 and py[k]<.125 and px[k]<.125: # Verifica se a sonda está dentro da plataforma com os pés fixados
+        action = 0 # Caso True, não necessidade de mais ajuste, pois o obejetivo já foi alcançado
+    return int(action) # Retorna a ação para aplicar no ambiente Lunar Lander
 
-params =  np.array([58.52230249, 23.10399406,  3.69236577])
-frames = []
-samples = 2000000
-px = np.zeros(samples)
-py = np.zeros(samples)
-p1 = np.zeros(samples)
-p2 = np.zeros(samples)
-at = np.zeros(samples)
-v = np.zeros(samples)
-vx= np.zeros(samples)
-vt = np.zeros(samples)
-j = []
-z = []
-env = gym.make("LunarLander-v2", render_mode="rgb_array")
-env.action_space.seed()
-observation, info = env.reset()
-r = observation[3]/observation[1]
-action = 0
-cont = 0
-point = 0
-g = []
-inicio = time.time()
-for i in range(samples):
-    px[i] = observation[0]*(np.cos(np.pi/4))+observation[1]*(np.sin(np.pi/4))
-    py[i] = observation[0]*(-np.sin(np.pi/4))+observation[1]*(np.cos(np.pi/4))
-    at[i] = observation[4]
-    v[i] = observation[3]
-    vx[i] = observation[2]
-    vt[i] = observation[5]
-    if i>0:
-        action = MPC(px, py, i, at, v, params, pe)
-    observation, reward, terminated, truncated, info = env.step(action)
-    pe = observation[6:]
-    z.append(reward)
-    if reward==100:
-        cont+=1
-    if terminated or truncated:
-        observation, info = env.reset()
-        j.append(reward)
-        g.append(i)
-        print(len(g))
-        if len(g)>999:
-            break
+params =  np.array([58.52230249, 23.10399406,  3.69236577]) # Parâmetros calculados via algoritmo Subida de Encosta
+num_episodios = 1000 # Número de episódios
+samples = int(500*num_episodios) # Número de etapas (amostras)
+px = np.zeros(samples) # Array para salvar a posição x (p_x)
+py = np.zeros(samples) # Array para salvar a posição y (p_y)
+p1 = np.zeros(samples) # Array para salvar a entrada u_1 (propulsor principal)
+p2 = np.zeros(samples) # Array para salvar a entrada u_2 (propulsor auxiliar)
+at = np.zeros(samples) # Array para salvar o ângulo (\theta)
+v = np.zeros(samples) # Array para salvar a velocidade linear em y (v_y)
+vx= np.zeros(samples) # Array para salvar a velocidade linear em x (v_x)
+vt = np.zeros(samples) # Array para salvar a velocidade angular (v_{\theta})
+r = [] # Lista para salvar a pontuação
+env = gym.make("LunarLander-v2", render_mode="rgb_array") # Criando o ambiente LunarLander e renderização, onde você pode alterar 'human' para 'rgb_array' ou 'ansi' dependendo do modo que deseja usar
+env.action_space.seed() # Define a semente (seed) para a geração de números aleatórios no espaço de ações do ambiente
+observation, info = env.reset() # Reinicia o ambiente de simulação (LunarLander)
+action = 0 # Seta a primeira ação
+cont = 0 # Contador
+g = [] # Lista que registra o término do episódio
+inicio = time.time() # Inicia a contagem do tempo
+for i in range(samples): # Percorre todo espaço amostral
+    px[i] = observation[0]*(np.cos(np.pi/4))+observation[1]*(np.sin(np.pi/4)) # Salva posição x (p_x) rotacionada em 45°
+    py[i] = observation[0]*(-np.sin(np.pi/4))+observation[1]*(np.cos(np.pi/4))  # Salva posição y (p_y) rotacionada em 45°
+    at[i] = observation[4] # Salva o ângulo (\theta)
+    v[i] = observation[3] # Salva a velocidade linear em y (v_y)
+    vx[i] = observation[2] # Salva a velocidade linear em x (v_x)
+    vt[i] = observation[5] # Salva a velocidade angular (v_{\theta})
+    if i>=2: # Verifica se há atraso suficiente (2) para o sistema começão atuar
+        action = MPC(px, py, i, at, v, params, pe) # Chama a função MPC com seus respectivos argumentos, que por sua vez retorna a ação de controle(action)
+    observation, reward, terminated, truncated, info = env.step(action) # Aplicação da ação de controle
+    pe = observation[6:] # Salva o posicionamento dos pés 
+    r.append(reward) # Guarda a recompensa por etapa
+    if reward==100: # Verifica se a sonda pousa
+        cont+=1 # Caso afirmativo, é contabilizado
+    if terminated or truncated: # Checa se o episódio terminou
+        observation, info = env.reset() # Reinicia o ambiente de simulação (LunarLander)
+        g.append(i) # Cada etapa de término é retida a lista g
+        print("Episódio número: ", len(g)) # O tamanho de g indica os episódios completos
+        if len(g)==int(num_episodios): # Checa se os episódios atingiram o número determinado
+            break # Encerra o ambiente
 fim = time.time()
-# Calcula o tempo decorrido
-tempo_decorrido = fim - inicio
-
+tempo_decorrido = fim - inicio # Calcula o tempo decorrido
 print(f"Tempo decorrido: {tempo_decorrido} segundos")
 env.close()
-print(cont)
+
+print("Número de pousos: ", cont)
+plt.plot(0, sum(r[:g[0]]), 'ko', label='Episódio')
+d = 0
+soma = np.zeros(1000)
+soma[0] = sum(r[:g[0]])
+# Plotando a pontuação de todos episódios
+for i in range(0, 999):
+    plt.plot(i+1, sum(r[g[i]:g[i+1]]), 'ko')
+    soma[i+1] = sum(r[g[i]:g[i+1]])
+    if sum(r[g[i]:g[i+1]])>200: # Verifica quais episódios possuem +200 pontos
+        d+=1
+print("Número de pousos com +200 pontos: ", d)
+print("Média de pontuação: ", np.mean(soma))
+plt.plot(np.ones(1000)*200, 'r',  linewidth=2.5, label='+200 pontos')
+plt.ylabel('Pontuação')
+plt.xlabel('Episódios')
+plt.title('Validação - GPC')
+plt.legend()
+plt.show()
